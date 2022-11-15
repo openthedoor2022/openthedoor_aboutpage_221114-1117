e1166733a1e0ac7742faf5c51906e9d4775b3458
gyuryeol jeon <wjsrbfuf@gmail.com>
2022-11-16 00:06:07 +0900

    수정 필요

--- gr_app.py
+++ gr_app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://team:sparta@cluster0.clqmmwt.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
+
 @app.route('/')
 def team():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/team", methods=["POST"])
 def team_post():
@@ -20,13 +24,14 @@ def team_post():
     }
 
     db.team.insert_one(doc)
-    return jsonify({'msg':'응원 완료!'})
+    return jsonify({'msg': '응원 완료!'})
+
 
 @app.route("/team", methods=["GET"])
 def team_get():
-    comment_list = list(db.team.find({},{'_id':False}))
-    return jsonify({'comments':comment_list})
+    comment_list = list(db.team.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
