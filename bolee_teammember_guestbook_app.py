d1cfb734e24a5266e9a0ad6b87e1a2938fbd0319
Bo LEE <85431891+boleesystem@users.noreply.github.com>
2022-11-17 15:06:23 +0900

    last commit

--- bolee_teammember_guestbook_app.py
+++ bolee_teammember_guestbook_app.py
@@ -1,4 +1,4 @@
-# 팀 소개 페이지용 방명록(포스트잇)
+# 팀원 소개 페이지용 방명록(포스트잇)
 # 2022.11.16. 구현 시작일 (코드 작성자 : 이보형) / 버전 2022.11.17.
 
 from flask import Flask, jsonify, render_template, request
@@ -11,30 +11,30 @@ db = client.bolee
 
 @app.route('/')
 def home():
-    return render_template('bolee_team_guestbook.html')
+    return render_template('bolee_teammember_guestbook.html')
 
 
-@app.route("/teamguestbook", methods=["GET"])
+@app.route("/teammemberguestbook", methods=["GET"])
 def bolee_get():
-    comment_list = list(db.teamguestbook.find({}, {'_id': False}))
+    comment_list = list(db.teammemberguestbook.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
 
 
-@app.route("/teamguestbook", methods=["POST"])
+@app.route("/teammemberguestbook", methods=["POST"])
 def bolee_post():
     usrname_receive = request.form['usrname_give']
     cntcomment_receive = request.form['cntcomment_give']
 
-    comment_list = list(db.teamguestbook.find({}, {'_id': False}))
+    comment_list = list(db.teammemberguestbook.find({}, {'_id': False}))
     count = len(comment_list) + 1
 
     doc = {
         'usrname': usrname_receive,
         'cntcomment': cntcomment_receive
     }
-    db.teamguestbook.insert_one(doc)
-    return jsonify({'msg': '똑똑똑! 당신의 소중한 이야기가 도착했습니다.'})
+    db.teammemberguestbook.insert_one(doc)
+    return jsonify({'msg': '똑똑똑! 소중한 말씀 감사합니다.'})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5002, debug=True)
